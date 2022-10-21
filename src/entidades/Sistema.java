7d77b6736539c2cd3c426817e436922d4f45ae62
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2022-10-21 19:23:44 -0400

    Projeto atualizado em 21/10/200 - 19h23

--- src/entidades/Sistema.java
+++ src/entidades/Sistema.java
@@ -239,7 +239,7 @@ public class Sistema {
             System.out.println("6 - Voltar Menu Anterior");
             System.out.print("Sua opção: ");
             int opcao = Integer.parseInt(entrada.nextLine());
-            
+
             switch (opcao) {
                 case 1:
                     System.out.println("\nNome: ");
@@ -248,7 +248,7 @@ public class Sistema {
                     int idade = Integer.parseInt(entrada.nextLine());
                     System.out.println("\nSexo: ");
                     char sexo = entrada.nextLine().charAt(0);
-                   
+
                     //vamos incrementar o contador de pessoas
                     Pessoa.contadorPessoas++;
                     //agora vamos criar um objeto da classe Pessoa
@@ -256,12 +256,212 @@ public class Sistema {
                     //e finalmente mostramos uma mensagem de sucesso.
                     System.out.println("\nA pessoa foi criada com sucesso.");
                     break;
-                default:
-                    throw new AssertionError();
+                case 2:// vamos listas as pessoas cadastradas
+                    if (pessoas.isEmpty()) {
+                        System.out.println("\nNão há nenhuma pessoa cadastrada.");
+                    } else {
+                        for (int i = 0; i < pessoas.size(); i++) {
+                            temp = pessoas.get(i);//obtém a pessoa da iteração atual
+                            System.out.println("\nId: " + temp.getId());
+                            System.out.println("\nNome: " + temp.getNome());
+                            System.out.println("\nIdade: " + temp.getIdade());
+                            System.out.println("\nSexo: " + temp.getSexo());
+                            System.out.println("Quantidade de Contas Bancárias: " + temp.getContas(bancos).size());
+                        }
+                    }
+                    break;
+                case 3: // vamos pesquisar uma pessoa
+                    System.out.print("\nInforme o id ou nome da pessoa: ");
+                    pesquisaPessoa = entrada.nextLine();
+                    //chamamos o método que pesquisa a pessoa
+                    temp = pesquisarPessoa(pesquisaPessoa);
+                    if (temp == null) {//pessoa não encontrada
+                        System.out.println("\nA pessao não encontrada.");
+                    } else // mostra e a pessoa encontrada
+                    {
+                        System.out.println("\nId: " + temp.getId());
+                        System.out.println("\nNome: " + temp.getNome());
+                        System.out.println("\nIdade: " + temp.getIdade());
+                        System.out.println("\nSexo: " + temp.getSexo());
+                        System.out.println("Quantidade de Contas Bancárias: " + temp.getContas(bancos).size());
+                    }
+                    break;
+                case 4: // vamos excluir uma pessoa
+                    System.out.print("\nInforme o id ou nome da pessoa a ser excluída: ");
+                    pesquisaPessoa = entrada.nextLine();
+                    //chamamos o método que pesquisa a pessoa
+                    temp = pesquisarPessoa(pesquisaPessoa);
+                    if (temp == null) {// pessoa não encontrada
+                        System.out.println("\nA pessoa não foi encontrada.");
+                    } else {// vamos excluir a pessoa. Atenção: Se houver contas bancárias relacionadas a esta pessoa, então a exclusão da conta bancária deverá ser feita primeiro
+                        if (temp.getContas(bancos).size() > 0) {
+                            System.out.println("\nOps! Esta pessoa possui contas bancárias . Exclua as contas primeiro.");
+                        } else {
+                            pessoas.remove(temp);
+                            System.out.println("\nPessoa excluída com sucesso");
+                        }
+                    }
+                    break;
+                case 5: // vamos atualizar uma pessoa
+                    System.out.print("\nInforme o id ou nome da pessoa a ser atualizada: ");
+                    pesquisaPessoa = entrada.nextLine();
+                    //chamamos o método que pesquisa a pessoa
+                    temp = pesquisarPessoa(pesquisaPessoa);
+                    if (temp == null) {//pessoa não encontrada
+                        System.out.println("\nA pessoa não foi encontrada");
+                    } else {//mostra a pessoa encontrada
+                        System.out.println("\nId: " + temp.getId());
+                        System.out.println("\nNome: " + temp.getNome());
+                        System.out.println("\nIdade: " + temp.getIdade());
+                        System.out.println("\nSexo: " + temp.getSexo());
+                        System.out.println("Quantidade de Contas Bancárias: " + temp.getContas(bancos).size());
+                        System.out.println("\nInforme os novos dados: ");
+                        System.out.print("\nNovo Nome:");
+                        String novoNomePessoa = entrada.nextLine();
+                        System.out.print("\nNova Idade:");
+                        int novaIdadePessoa = Integer.parseInt(entrada.nextLine());
+                        System.out.print("\nNovo sexo: ");
+                        char novoSexoPessoa = entrada.nextLine().charAt(0);
+//vamos atualizar os dados desta pessoa no ArrayList
+                        temp.setNome(novoNomePessoa);
+                        temp.setId(novaIdadePessoa);
+                        temp.setSexo(novoSexoPessoa);
+                    }
+                    break;
+                case 6:
+                    return 0; // volta para o menu principal
             }
-            
+
         }
-        
+
+    }
+
+    //método que pesquisa uma pessoa pelo id, número ou nome e retorna um objeto da classe Pessoa
+    public Pessoa pesquisarPessoa(String pesquisaPessoa) {
+        Pessoa p = null;
+        //esta pessoa exite?
+        for (int i = 0; i < pessoas.size(); i++) {
+            //pesquisa pelo id
+            if (Integer.toString(pessoas.get(i).getId()).equals(pesquisaPessoa)) {
+                return pessoas.get(i);
+            } else if (pessoas.get(i).getNome().contains(pesquisaPessoa)) {//pesquisar por nome
+                return pessoas.get(i);
+            }
+        }
+        return p;
+    }
+
+    //menu para cadastrar, listar, pesquisar, excluir e atualizar as agências
+    public int menuGerenciarAgencias() {
+        Agencia temp; // serve para várias operações neste menu 
+        String pesquisaAgencia; // serve para as pesquisas das agências
+        Banco bancoAtual = null; // guarda o banco atual
+        //para gerenciar uma agência nós precisamos de um banco 
+        while (bancoAtual) {
+            System.out.println("\nInforme o id, número ou nome do banco: ");
+            String pesquisaBanco = entrada.nextLine();
+            //chamamos o método que pesquisa o banco
+            Banco b = pesquisarBanco(pesquisaBanco);
+            if (b == null) {//banco não encontrado
+                System.out.println("\nO banco não foi encontrado. \n\nDigite 1 para pesquisar novamente ou 2 para voltar ao menu anterior:");
+                int opcao = Integer.parseInt(entrada.nextLine());
+                if (opcao == 2) {
+                    return 1;// saímos daqui e voltamos para o menu anterior
+                }
+            } else {//banco encontrado. Vamos prosseguir com as agências
+                bancoAtual = b;
+            }
+        }
+        //Atenção: o menu abaixo deverá se exibido somente se um banco for selecionado
+        while (true) {//monstra o menu de forma repetitiva até o usuário usar a opção de sair
+            System.out.println("\n:: G E R E N C I A R  A G Ê N C I A ::\n");
+            System.out.println("Banco Selecionado: " + bancoAtual.getNome() + "\n");
+
+            System.out.println("Escolha o opção desejada");
+            System.out.println("1 - Nova Agência");
+            System.out.println("2 - Listar Agências");
+            System.out.println("3 - Pesquisar Agência");
+            System.out.println("4 - Excluir Agência");
+            System.out.println("5 - Atualizar Agência");
+            System.out.println("6 - Voltar ao menu anterior");
+            System.out.print("Sua opção: ");
+            int opcao = Integer.parseInt(entrada.nextLine());//lê a opção do usuário
+
+            switch (opcao) {
+                case 1://vamos cadastrar uma nova agência
+                    System.out.print("\nNúmero da Agência:");
+                    String numeroAgencia = entrada.nextLine();
+                    System.out.print("Cidade/Estado: ");
+                    String cidadeAgencia = entrada.nextLine();
+
+                    //vamos incrementar o contador de agências
+                    Agencia.contadorAgencias++;
+
+                    //agora vamos criar um objeto da classe Agênca
+                    Agencia a = new Agencia(bancoAtual, Agencia.contadorAgencias, numeroAgencia, cidadeAgencia);
+                    //e o adicionamos no ArrayList de agencias do banco selecionado
+                    bancoAtual.getAgencias().add(a);
+                    //e finalmente mostramos uma mensagem de sucesso.
+                    System.out.println("\nA Agência foi criada com sucesso.");
+                    break;
+                case 2: //vamos listar agências cadastradas no banco selecionado
+                    if (bancoAtual.getAgencias().isEmpty()) {
+                        System.out.println("\nNão há nenhuma agência cadastrada neste banco.");
+                    } else {
+                        for (int i = 0; i < bancoAtual.getAgencias().size(); i++) {
+                            temp = bancoAtual.getAgencias().get(i); // obtém a agência da iteração atual
+                            System.out.println("\nId: " + temp.getId());
+                            System.out.println("\nNúmero: " + temp.getNumero());
+                            System.out.println("\nCidade/Estado: " + temp.getCidade());
+                            System.out.println("\nQuantas Contas Bancárias: " + temp.getContas().size());
+                        }
+                    }
+                    break;
+                case 3: //vamos pesquisar uma agência
+                    System.out.print("\nInforme o id, número ou cidade da agência: ");
+                    pesquisaAgencia = entrada.nextLine();
+                    //chamamos o método que pesquisa a agência
+                    temp = pesquisarAgencia(bancoAtual, pesquisaAgencia);
+                    if (temp == null) {//agência não encontrada
+                        System.out.println("\nA agência não foi encontrada");
+                    } else {//mostra agência encontrada
+                        System.out.println("\nId: " + temp.getId());
+                        System.out.println("\nNúmero: " + temp.getNumero());
+                        System.out.println("\nCidade/Estado: " + temp.getCidade());
+                        System.out.println("\nQuantas Contas Bancárias: " + temp.getContas().size());
+                    }
+                    break;
+                case 4:// vamos excluir uma agência
+
+                    System.out.print("\nInforme o id, número ou cidade da agência");
+                    pesquisaAgencia = entrada.nextLine();
+                    //chamamos o método que pesquisa a agência
+                    temp = pesquisarAgencia(bancoAtual, pesquisaAgencia);
+                    if (temp == null) {//agência não encontrada
+                        System.out.println("\nAgência não foi encontrada");
+                    } else {//vamos excluir esta agência. Atenção. Ao excluir uma agência, todas suas contas serão excluídas também.
+                        bancoAtual.getAgencias().remove(temp);
+                        System.out.println("\nAgência excluída com sucesso.");
+                    }
+                    break;
+
+                case 5: //vamos atualizar uma agência
+                    System.out.println("\nInforme o id, número ou cidade da agência: ");
+                    pesquisaAgencia = entrada.nextLine();
+                    //chamamos o método que pesquisa a agência
+                    temp = pesquisarAgencia(bancoAtual, pesquisaAgencia);
+                    if (temp == null) {//agência não encontrada
+                        System.out.println("\nA agência não foi encontrada");
+                    } else {//mostra a agêcia encontrada
+                        System.out.println(
+                        "n\Dados atuais desta agência:");
+                              System.out.println("\nId: " + temp.getId());
+                        System.out.println("\nNúmero: " + temp.getNumero());
+                        System.out.println("\nCidade/Estado: " + temp.getCidade());
+                        System.out.println("\nQuantas Contas Bancárias: " + temp.getContas().size());
+                    }
+            }
+        }
     }
 
 }
